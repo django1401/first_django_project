27dfc5995fe2e18d3ee3a8c3b0c05a1e53e7644e
django1401 <126799693+django1401@users.noreply.github.com>
2023-03-05 18:28:36 +0330

    create first app
    first django app and html render file

--- config/viwes.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-
-
-def home(req):
-    return HttpResponse("hello first page")
-
-def contact(req):
-    return HttpResponse("contact_Us")
---
+++ home/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+def home(req):
+    return render(req,"home/test1.html")
+
+
+
+
+    
+
+
