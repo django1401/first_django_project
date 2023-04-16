4913cfc3f123263820ee39cb74fc14b82b018aed
django1401 <126799693+django1401@users.noreply.github.com>
2023-04-16 18:00:37 +0330

    create data model
    model admin

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from .views import *
 
+app_name = 'home'
+
 urlpatterns = [
-    path('',home),
-    path('about',about),
-    path('contact',contact),
+    path('',home, name='home'),
+    path('about',about, name='about'),
+    path('contact',contact , name='contact'),
+    path('blog',blog, name= 'blog'),
+    path('blog-single',blog_single, name='blog-single'),
+
 ]
--- home/views.py
+++ home/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 
 def home(req):
-    return render(req,"home/index.html")
+    return render(req,"home/index.html",context={'name':'hamid reza'})
 
 def about(req):
     return render(req,"home/about.html")
 
 def contact(req):
     return render(req,"home/contact.html")
+
+def blog(req):
+    return render(req,"home/blog-home.html")
+
+def blog_single(req):
+    return render(req,"home/blog-single.html")
 
 
 
