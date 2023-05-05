cf3ed7c9a367085f0ff95cabb78be4fef03b060a
neg._.ah <127397572+neg2004ah@users.noreply.github.com>
2023-05-05 15:07:28 +0430

    published_date__lte = timezone.now()
    update model Post

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
-from .models import Post
 
-# Register your models here.
+from .models import Post,Category,Tags
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['title','status', 'created_date']
+    list_display = ['title','status','created_date']
     list_filter = ['status']
-    search_fields = ['title']
-    empty_value_display = 'empty_value'
+    search_fields = ['status']
+    empty_value_display = 'empty-value'
+    
+admin.site.register(Category)
+admin.site.register(Tags)
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
-from .models import Post
-
-# Create your views here.
-
+from django.utils import timezone
+from .models import *
 
 
-def blog_home(req):
-    posts = Post.objects.filter(status=True)
+def blog_home(request):
+    posts = Post.objects.filter(status= True,published_date__lte = timezone.now())
+    
     context = {
         'posts' : posts
     }
-    return render(req, 'blog/blog-home.html', context=context)
+    return render(request , 'blog/blog-home.html',context = context)
+
+def blog_single(request):
+    return render(request , 'blog/blog-single.html')
