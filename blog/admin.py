95753e67c3d546becbc4a8a2f3eea34e5229042e
neg._.ah <127397572+neg2004ah@users.noreply.github.com>
2023-05-05 15:16:25 +0430

    Update admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -6,7 +6,7 @@ from .models import Post,Category,Tags
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title','status','created_date']
     list_filter = ['status']
-    search_fields = ['status']
+    search_fields = ['status','title']
     empty_value_display = 'empty-value'
     
 admin.site.register(Category)
